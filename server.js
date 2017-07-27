77a7cbea58201988957b2a9892b9a32f99ec9f40
Ken Wong <KenWong@Kens-MacBook-Pro.local>
2017-07-27 00:54:52 -0700

    added css to public folder

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var PORT = process.env.PORT || 8080;
 // The code below is pretty standard.
 
 // Serve static content for the app from the "public" directory in the application directory.
-//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
